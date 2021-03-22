56a06ea1e753aba72907069a4b7719a9d9104d0c
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-03-22 12:24:12 +0100

    Update solution

--- 1-es-modules-syntax/main.js
+++ 1-es-modules-syntax/main.js
@@ -1,16 +1,16 @@
-export default function sum(a, b) {
+function sum(a, b) {
   return a + b;
 }
 
-export function substract(a, b) {
+function substract(a, b) {
   return a - b;
 }
 
-export function multiply(a, b) {
+function multiply(a, b) {
   return a * b;
 }
 
-export function divide(a, b) {
+function divide(a, b) {
   return a / b;
 }
 
