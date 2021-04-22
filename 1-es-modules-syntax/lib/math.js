4af690fa920009619669dd353406c6ffbe765776
Thomas Kirst <thomas.kirst@neuefische.de>
2021-04-22 14:34:25 +0200

    Added solution

---
+++ 1-es-modules-syntax/lib/math.js
@@ -0,0 +1,17 @@
+export default function sum(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  return a / b;
+}
+
+export const numberToExport = 1;
--- 1-es-modules-syntax/main.js
+++ 1-es-modules-syntax/main.js
@@ -1,20 +1,13 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+import sum, {
+  subtract,
+  multiply,
+  divide,
+  numberToExport as importedNumber,
+} from './lib/math';
 
 console.log(sum(4, 5));
 console.log(subtract(8, 4));
 console.log(multiply(6, 4));
 console.log(divide(8, 2));
+
+console.log(importedNumber, 'Imported number');
